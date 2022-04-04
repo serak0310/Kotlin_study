fce5515d4457f784560ba642631b27048d6db60c
serak0310 <serak0310@naver.com>
2022-04-04 16:35:50 +0900

    람다, 확장함수

--- GrammerBasic/app/src/main/java/com/example/kotlingrammerbasic/advancedSummary1.kt
+++ GrammerBasic/app/src/main/java/com/example/kotlingrammerbasic/advancedSummary1.kt
@@ -17,12 +17,39 @@ val nameAge = {name : String, age : Int ->
 }
 
 
-//확장함수
-//ex) 스트링 클래스에 기능을 추가할 때
+// 확장함수
+// 기존 클래스의 함수를 확장
+// ex) 스트링 클래스에 기능을 추가할 때
 val pizzaIsGreat : String.() -> String = {
     this + "Piszza is BEST!!"
 }
 
+fun extendString(name:String, age:Int) : String {
+    val introduceMyself : String.(Int) -> String = {"I'm ${this} and ${it} years old"}
+    // this=확장함수가 부르는 오브젝트인 String
+    // it = Int, Int 딱 하나만 파라미터이기 때문에
+    return name.introduceMyself(age)
+}
+
+
+// 람다의 Return
+// 마지막 구현식을 return함
+// input 파라미터는 여러개 가 올 수 있으므로 괄호 꼭 해줘야 함.
+val calculateGrade : (Int) -> String = {
+    when(it) {
+        in 0..40 -> "fail"
+        in 41..70 -> "pass"
+        in 71..100 -> "perfect"
+        else -> "Error"
+    }
+
+}
+
+// 람다를 표현하는 여러가지 방법
+// 람다식을 파라미터로 받기
+fun invokeLamda(lamda : (Double)->Boolean) : Boolean {
+    return lamda(5.234)
+}
 
 fun main(){
     println(square(12))
@@ -30,5 +57,17 @@ fun main(){
 
     val a = "minjoeng said "
     println(a.pizzaIsGreat())
+
+    println(extendString("ariana", 28))
+
+    println(calculateGrade(90))
+
+    val lamda = {number:Double ->
+        number == 4.3213
+    }
+    println(invokeLamda(lamda))
+    println(invokeLamda({ it>3.24 }))
+    println(invokeLamda{ it>3.24 })     //마지막 파라미터가 람다식일 때 괄호 생략 가능
+
 }
 
